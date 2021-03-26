cdfad976382efe9c496f33842c44ecbddb0a3356
Gaurav Tiwari <gaurav1@tutamail.com>
2021-03-26 16:03:38 +0530

    Implement model for encrypted data

--- source/models/analytics_data_model.hpp
+++ source/models/analytics_data_model.hpp
@@ -1,8 +1,12 @@
 #ifndef __ANALYTICS_DATA_MODEL_H__
 #define __ANALYTICS_DATA_MODEL_H__
 
+#include <spdlog/fmt/ostr.h>
+
+#include <algorithm>
 #include <nlohmann/json.hpp>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace prinsight {
@@ -10,30 +14,43 @@ namespace prinsight {
   private:
     /* data */
     std::size_t index;
-    std::string encData;
-    std::vector<std::string> feKey;
+    std::unordered_map<std::string, std::string> encData;
+    std::vector<std::string> fdKey;
 
   public:
-    AnalyticsData() : index(0), encData("") {
-      feKey.push_back("");
-      feKey.push_back("");
-    }
+    AnalyticsData() = default;
 
-    AnalyticsData(std::size_t index, const std::string encData,
+    AnalyticsData(std::size_t index, const std::unordered_map<std::string, std::string> encData,
                   const std::vector<std::string> feKey)
-        : index(index), encData(encData), feKey(feKey) {}
+        : index(index), encData(encData), fdKey(feKey) {}
     ~AnalyticsData() = default;
 
     inline std::size_t getIndex() const { return index; }
     inline void setIndex(std::size_t index) { this->index = index; }
 
-    inline std::string getEncData() const { return encData; }
-    inline void setEncData(const std::string& encData) { this->encData = encData; }
+    inline std::unordered_map<std::string, std::string> getEncData() const { return encData; }
+    inline void setEncData(const std::unordered_map<std::string, std::string> &encData) {
+      this->encData = encData;
+    }
 
-    inline std::vector<std::string> getFeKey() const { return feKey; }
-    inline void setFeKey(const std::vector<std::string>& feKey) { this->feKey = feKey; }
+    inline std::vector<std::string> fdKey() const { return fdKey; }
+    inline void setFeKey(const std::vector<std::string> &feKey) { this->fdKey = feKey; }
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(AnalyticsData, index, encData, feKey)
+    template <typename OStream> friend OStream &operator<<(OStream &os, const AnalyticsData &a) {
+      os << "analytics data [index:" << a.index << ", enc-data:{";
+      for (auto element : a.encData) {
+        os << "{" << element.first << ", " << element.second << "},";
+      }
+      os << "}, fekey:{";
+      for (auto element : a.fdKey) {
+        os << element << ",";
+      }
+      os << "}]" << std::endl;
+
+      return os;
+    }
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(AnalyticsData, index, encData, fdKey)
   };
 }  // namespace prinsight
 
